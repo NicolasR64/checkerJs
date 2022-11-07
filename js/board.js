b15588b95e81dabe21b96b04e23d36249eba9513
NicolasR64 <nicolasravaux64@gmail.com>
2022-11-07 14:29:14 +0100

    finish display board

--- js/board.js
+++ js/board.js
@@ -1,6 +1,6 @@
 // generate board
-function generateBoard(){
-    console.log('generate board');
+function generateBoard(player1, player2){
+    console.log('generating board...');
     let container = document.querySelector("#board-container");
     container.innerHTML = '';
 
@@ -18,25 +18,48 @@ function generateBoard(){
     for(let row=0; row < 8; row++){
         for(let col=0; col < 8; col++){
             if((col + row) % 2 == 1){
-                displayedBoard += `<div id="${row}${col}" class="case black">${row}${col} </div>`;
+                displayedBoard += `<div id="case${row}${col}" class="case black"><div class="pion"></div></div>`;
                 if(row < 3){
-                    piece[row][col] = 1;
+                    piece[row][col] = new pion(Number(`${row}${col}`), player1, 1);
                 }else if(row > 4){
-                    piece[row][col] = 2;
+                    piece[row][col] = new pion(Number(`${row}${col}`), player2, 1);
                 }else{
-                    piece[row][col] = 0;
+                    piece[row][col] = new pion(Number(`${row}${col}`), null, 0);
                 }
             }else{
-                displayedBoard += `<div class="case white" style="color: black;">${row}${col}</div>`
-                piece[row][col] = 0;
+                displayedBoard += `<div id="case${row}${col}"  class="case white" style="color: black;"><div class="pion"></div></div>`
+                piece[row][col] = new pion(Number(`${row}${col}`), null, 0);
             }
         }
     }
     console.table(piece);
 
-
     container.innerHTML += displayedBoard;
+    displayPion(piece);
+
+    //add event listener
+    for(let row=0; row < 8; row++){
+        for(let col=0; col < 8; col++){
+            document.querySelector(`#case${id} .pion`).addEventListener('click', move.checkMoveExist);
+        }
+    }
+}
+
+function displayPion(piece){
+    for(let row=0; row < 8; row++){
+        for(let col=0; col < 8; col++){
+            if(piece[row][col].owner != null) {
+                piece[row][col].displayPion(`${row}${col}`, piece[row][col].owner.color);
+            };
+        }
+    }
+}
+
+function generatePlayer(){
+    let player1 = new owner(1, "red", "nicolas");
+    let player2 = new owner(2, "yellow", "megane");
+    generateBoard(player1, player2);
 }
 
 let target = document.querySelector("#startGame");
-target.addEventListener('click', generateBoard);
+target.addEventListener('click', generatePlayer);
